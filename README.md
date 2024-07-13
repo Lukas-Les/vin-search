b90455b34db7432592c3c2ac175457568d52f3cc
stabmeldys <lukas.les93@gmail.com>
2024-07-13 19:37:13 +0300

    readme and dir parsing

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # vin-search
+
+## Usage
+
+`go run . -f <target file> -o <output file>`
+
+or
+
+`go run . -d <target dir> -o <output file>`
+
+Run `go run . -h` for flags explained
+
+## Output
+
+Output format is `<file path>-><results> <space> <separated>`
+
+If output file (-o) is not provided, prints result to console
--- src/main.go
+++ src/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 	"regexp"
 )
 
@@ -15,14 +16,20 @@ func check(e error) {
 
 func main() {
 	var targetFilePath string
+	var targetDirPath string
 	var outputFilePath string
 	var vinRegex string
 	var maxResult int
 	flag.StringVar(&targetFilePath, "f", "", "location of the target file")
+	flag.StringVar(&targetDirPath, "d", "", "location of the target directory")
 	flag.StringVar(&outputFilePath, "o", "", "location of the output file")
-	flag.StringVar(&vinRegex, "v", "(?i)[A-HJ-NPR-Z0-9]{17}", "regular expression pattern for a VIN")
+	flag.StringVar(&vinRegex, "v", "(?i)\\b[A-HJ-NPR-Z0-9]{17}\\b", "regular expression pattern for a VIN")
 	flag.IntVar(&maxResult, "max", -1, "max number of vin numbers to extract")
 	flag.Parse()
+
+	if targetDirPath == "" && targetFilePath == "" {
+		panic("target file or directory not provided! Use -f <target file> or -d <target dir>")
+	}
 
 	var output []string
 
@@ -35,6 +42,15 @@ func main() {
 				search(string(f), vinRegex, maxResult),
 			)}
 	}
+	if targetDirPath != "" {
+		var targetFilesPaths []string = collectTargetFilesPaths(targetDirPath)
+		for _, filePath := range targetFilesPaths {
+			f, err := os.ReadFile(filePath)
+			check(err)
+			output = append(output, formatOutput(filePath, search(string(f), vinRegex, maxResult)))
+		}
+	}
+
 	if outputFilePath != "" {
 		writeOutput(output, outputFilePath)
 	} else {
@@ -42,6 +58,27 @@ func main() {
 			fmt.Println(line)
 		}
 	}
+}
+
+func collectTargetFilesPaths(targetDirPath string) []string {
+	var filePaths []string
+
+	err := filepath.Walk(targetDirPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !info.IsDir() {
+			filePaths = append(filePaths, path)
+		}
+		return nil
+	})
+
+	if err != nil {
+		fmt.Printf("Error walking the path %v: %v\n", targetDirPath, err)
+	}
+
+	return filePaths
 }
 
 func search(content string, vinRegex string, maxResult int) []string {
